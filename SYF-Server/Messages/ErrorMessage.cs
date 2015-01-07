e04573a9b4a418d9f1b5eae2203e13caee9fe81c
rinukkusu <rinukkusu@sub-r.de>
2015-01-07 03:40:29 +0100

    [Messages] added basic Message classes

---
+++ SYF-Server/Messages/ErrorMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace SYF_Server.Messages
+{
+    enum ErrorCode
+    {
+
+    }
+
+    [DataContractAttribute]
+    public class ErrorMessage : TextMessage
+    {
+        [DataMemberAttribute]
+        public ErrorCode Code;
+    }
+}
