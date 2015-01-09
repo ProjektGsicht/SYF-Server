01c5ad9c79cf2e2a10aa9ad3066478be059a1ec7
rinukkusu (Laptop) <rinukkusu@sub-r.de>
2015-01-09 14:17:00 +0100

    [Misc] added static constants class

---
+++ SYF-Server/Misc/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SYF_Server.Misc
+{
+    class Constants
+    {
+        public const string DB_USERS                        = "users";
+        public const string DB_USERS_FIELD_ID               = "id";
+        public const string DB_USERS_FIELD_NAME             = "name";
+        public const string DB_USERS_FIELD_USERNAME         = "username";
+        public const string DB_USERS_FIELD_PASSWORD         = "password";
+
+        public const string DB_FACEIMAGES                   = "faceimages";
+        public const string DB_FACEIMAGES_FIELD_ID          = "id";
+        public const string DB_FACEIMAGES_FIELD_FACEIMAGE   = "faceimage";
+    }
+}
