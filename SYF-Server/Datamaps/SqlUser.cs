3e1350041ca3cf80f937d6a4d7e916a37bb2b6a5
rinukkusu (Laptop) <rinukkusu@sub-r.de>
2015-01-09 14:15:24 +0100

    [Datamaps] added basic SqlUser for later on

---
+++ SYF-Server/Datamaps/SqlUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SYF_Server.Datamaps
+{
+    class SqlUser
+    {
+        public SqlUser(int Id)
+        {
+            this._Id = Id;
+        }
+
+        private int _Id;
+        public int Id
+        {
+            get
+            {
+                return _Id;
+            }
+        }
+
+        public string Username;
+        public string Name;
+        public string Password;
+    }
+}
