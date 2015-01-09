8476ce3f2801368542c90b9da5b528b464cd54fe
rinukkusu (Laptop) <rinukkusu@sub-r.de>
2015-01-09 11:19:05 +0100

    [Validation] basic implementation of the interface

---
+++ SYF-Server/Validation/FingerprintValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SYF_Server.Validation
+{
+    class FingerprintValidator : IValidator
+    {
+        public bool Validate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Messages.ErrorMessage LastErrorMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SYF-Server/Validation/IValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SYF_Server.Messages;
+
+namespace SYF_Server.Validation
+{
+    interface IValidator
+    {
+        bool Validate();
+        ErrorMessage LastErrorMessage();
+    }
+}
