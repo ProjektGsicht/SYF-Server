409cff1a32ba63fb84f3aecf7e7e14157267e366
rinukkusu (Laptop) <rinukkusu@sub-r.de>
2015-01-13 06:40:51 +0100

    [Validation] implemented basic FaceImageValidator

--- SYF-Server/Validation/FaceImageValidator.cs
+++ SYF-Server/Validation/FaceImageValidator.cs
@@ -2,14 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SYF_Server.Datamaps;
+using System.Drawing;
+using Emgu;
+using Emgu.CV;
+using Emgu.Util;
 
 namespace SYF_Server.Validation
 {
     class FaceImageValidator : IValidator
     {
+        private SqlUser _User;
+        public SqlUser User
+        {
+            get
+            {
+                return _User;
+            }
+        }
+
+        private Bitmap _Image;
+        public Bitmap Image
+        {
+            get
+            {
+                return _Image;
+            }
+        }
+
         public bool Validate()
         {
-            throw new NotImplementedException();
+            FisherFaceRecognizer rec = new FisherFaceRecognizer(0, 0);
+
+            List<EmguImage> Images = new List<EmguImage>();
+            List<int> Labels = new List<int>();
+
+            int i = 0;
+            foreach (Bitmap Image in User.FaceImages)
+            {
+                Images.Add(new EmguImage(Image));
+                Labels.Add(i);
+                i++;
+            }
+
+            rec.Train(Images.ToArray(), Labels.ToArray());
+
+            FaceRecognizer.PredictionResult result = rec.Predict(new EmguImage(this.Image));
+            result.GetType();
+
+            if (result.Label > 0)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
         }
 
         public Messages.ErrorMessage LastErrorMessage()
