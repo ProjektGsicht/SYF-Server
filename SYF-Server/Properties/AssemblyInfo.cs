0d5015bdfa0243a344feca21aa9b3cbb0b2c42a8
rinukkusu (Laptop) <rinukkusu@sub-r.de>
2015-01-13 06:49:15 +0100

    [Misc] added EmguCV libs to the project folder

--- SYF-Server/Properties/AssemblyInfo.cs
+++ SYF-Server/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.64.47025")]
-[assembly: AssemblyFileVersion("1.0.64.47025")]
+[assembly: AssemblyVersion("1.0.65.47025")]
+[assembly: AssemblyFileVersion("1.0.65.47025")]
