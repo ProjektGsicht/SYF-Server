821ffd8313429950e1b4c38b19a3c2b0ba1d99f3
rinukkusu <rinukkusu@sub-r.de>
2015-01-21 13:23:18 +0100

    [Misc] edited table and column names for release

--- SYF-Server/Misc/Constants.cs
+++ SYF-Server/Misc/Constants.cs
@@ -7,16 +7,16 @@ namespace SYF_Server.Misc
 {
     class Constants
     {
-        public const string DB_USERS                        = "users";
-        public const string DB_USERS_FIELD_ID               = "id";
+        public const string DB_USERS                        = "tb1_userdaten";
+        public const string DB_USERS_FIELD_ID               = "userid";
         public const string DB_USERS_FIELD_NAME             = "name";
         public const string DB_USERS_FIELD_USERNAME         = "username";
         public const string DB_USERS_FIELD_WINDOWSUSER      = "windowsuser";
         public const string DB_USERS_FIELD_PASSWORD         = "password";
 
-        public const string DB_FACEIMAGES                   = "faceimages";
-        public const string DB_FACEIMAGES_FIELD_ID          = "id";
-        public const string DB_FACEIMAGES_FIELD_USERID      = "userid";
-        public const string DB_FACEIMAGES_FIELD_FACEIMAGE   = "faceimage";
+        public const string DB_FACEIMAGES                   = "tb2_verifikation1";
+        public const string DB_FACEIMAGES_FIELD_ID          = "ID";
+        public const string DB_FACEIMAGES_FIELD_USERID      = "Userid";
+        public const string DB_FACEIMAGES_FIELD_FACEIMAGE   = "Upload";
     }
 }
